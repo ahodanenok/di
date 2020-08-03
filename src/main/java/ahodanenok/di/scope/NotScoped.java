47537747252bff25b698706e2b970a27c792ab57
Andrei Hodanenok <xoda91@gmail.com>
2020-08-03 22:15:56 +0300

    allow @NotScoped to be used on fields

--- src/main/java/ahodanenok/di/scope/NotScoped.java
+++ src/main/java/ahodanenok/di/scope/NotScoped.java
@@ -7,7 +7,7 @@ import java.lang.annotation.*;
  * Dependencies with this scope are created every time they requested.
  */
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD}) // todo: field
 @Documented
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface NotScoped { }
